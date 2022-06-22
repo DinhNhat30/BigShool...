038b0ee4054cc1debed98f4a2ff00df93ecf76f0
84326 <84326@DESKTOP-88A959Q>
2022-06-22 23:20:23 +0700

    Add project files.

---
+++ DinhHoangNhat/Controllers/_dbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DinhHoangNhat.Controllers
+{
+    internal class _dbContext
+    {
+        internal static void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
